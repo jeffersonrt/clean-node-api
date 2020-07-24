13017528fd2dd77c6f9728f84559b98ccb307e5d
Jefferson Tora <hi@jeffersontora.com>
2020-07-24 09:27:37 -0300

    refactor: move EmailValidatorAdapter to the main layer

--- src/main/adapters/express/express-route-adapter.ts
+++ src/main/adapters/express/express-route-adapter.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from 'express'
-import { Controller, HttpRequest } from '../../presentation/protocols'
+import { Controller, HttpRequest } from '../../../presentation/protocols'
 
 export const adaptRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
-    const httpResquest: HttpRequest = {
+    const httpRequest: HttpRequest = {
       body: req.body
     }
 
-    const httpResponse = await controller.handle(httpResquest)
+    const httpResponse = await controller.handle(httpRequest)
     if (httpResponse.statusCode === 200) {
       res.status(httpResponse.statusCode).json(httpResponse.body)
     } else {
