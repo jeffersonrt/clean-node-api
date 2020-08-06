1c25501c516a572aa534f5e29b19b82e1e337417
Jefferson Tora <hi@jeffersontora.com>
2020-08-06 15:14:59 -0300

    test: ensure AuthMiddleware calls LoadAccountByToken with correct token

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,11 +1,44 @@
 import { forbidden } from '../helpers/http/http-helper'
 import { AccessDenied } from '../errors'
 import { AuthMiddleware } from './auth-middleware'
+import { AccountModel } from '../../domain/models/account'
+import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
+
+const makeFakeAccount = (): AccountModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_mail@mail.com',
+  password: 'valid_password'
+})
 
 describe('Auth Middleware', () => {
   test('Should return 403 if no x-access-token exists in headers', async () => {
-    const sut = new AuthMiddleware()
+    class LoadAccountByTokenStub implements LoadAccountByToken {
+      async load (token: string, role?: string): Promise<AccountModel> {
+        return await new Promise(resolve => resolve(makeFakeAccount()))
+      }
+    }
+    const loadAccountByToken = new LoadAccountByTokenStub()
+    const sut = new AuthMiddleware(loadAccountByToken)
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(forbidden(new AccessDenied()))
+  })
+
+  test('Should call LoadAccountByToken with correct AccessToken', async () => {
+    class LoadAccountByTokenStub implements LoadAccountByToken {
+      async load (token: string, role?: string): Promise<AccountModel> {
+        return await new Promise(resolve => resolve(makeFakeAccount()))
+      }
+    }
+
+    const loadAccountByToken = new LoadAccountByTokenStub()
+    const loadSpy = jest.spyOn(loadAccountByToken, 'load')
+    const sut = new AuthMiddleware(loadAccountByToken)
+    await sut.handle({
+      headers: {
+        'x-access-token': 'any_token'
+      }
+    })
+    expect(loadSpy).toBeCalledWith('any_token')
   })
 })
