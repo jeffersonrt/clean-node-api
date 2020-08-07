337b209f6e4a313fdca72d67492e5ecf9ee78ad1
Jefferson Tora <hi@jeffersontora.com>
2020-08-07 14:58:32 -0300

    refactor: organize main layer folders

---
+++ src/main/factories/controllers/login/signup/signup-controller-factory.ts
@@ -0,0 +1,15 @@
+import { SignUpController } from '../../../../../presentation/controllers/login/signup/signup-controller'
+import { makeDbAddAccount } from '../../../usecases/account/add-account/db-add-account-factory'
+import { Controller } from '../../../../../presentation/protocols'
+import { makeSignUpValidation } from './signup-validation-factory'
+import { makeDbAuthentication } from '../../../usecases/authentication/db-authentication-factory'
+import { makeLogControllerDecorator } from '../../../decorators/log-controller-decorator-factory'
+
+export const makeSignUpController = (): Controller => {
+  const signUpController = new SignUpController(
+    makeDbAddAccount(),
+    makeSignUpValidation(),
+    makeDbAuthentication()
+  )
+  return makeLogControllerDecorator(signUpController)
+}
