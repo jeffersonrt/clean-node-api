b0097f3937a80abe5620e1af3dfdc0c5d8213987
Jefferson Tora <hi@jeffersontora.com>
2020-08-07 14:17:12 -0300

    test: ensure JwtAdapter throws if verify throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -38,20 +38,29 @@ describe('Jwt Adapter', () => {
       const promise = sut.encrypt('any_id')
       await expect(promise).rejects.toThrow()
     })
+  })
 
-    describe('verify()', () => {
-      test('Should call verify with correct values', async () => {
-        const sut = makeSut()
-        const verifySpy = jest.spyOn(jwt, 'verify')
-        await sut.decrypt('any_token')
-        expect(verifySpy).toBeCalledWith('any_token', 'secret')
-      })
+  describe('verify()', () => {
+    test('Should call verify with correct values', async () => {
+      const sut = makeSut()
+      const verifySpy = jest.spyOn(jwt, 'verify')
+      await sut.decrypt('any_token')
+      expect(verifySpy).toBeCalledWith('any_token', 'secret')
+    })
 
-      test('Should return a value on verify success', async () => {
-        const sut = makeSut()
-        const value = await sut.decrypt('any_token')
-        expect(value).toBe('any_value')
+    test('Should return a value on verify success', async () => {
+      const sut = makeSut()
+      const value = await sut.decrypt('any_token')
+      expect(value).toBe('any_value')
+    })
+
+    test('Should throw if verify throws', async () => {
+      const sut = makeSut()
+      jest.spyOn(jwt, 'verify').mockImplementationOnce(() => {
+        throw new Error()
       })
+      const promise = sut.decrypt('any_token')
+      await expect(promise).rejects.toThrow()
     })
   })
 })
