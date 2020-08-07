f7ee9dfd1388b65e7dc316595a149302c978f90e
Jefferson Tora <hi@jeffersontora.com>
2020-08-07 17:09:16 -0300

    test: ensure add survey route returns 204 if  access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,9 +1,12 @@
 import request from 'supertest'
 import app from '../config/app'
-import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import env from '../config/env'
 
 let surveyCollection: Collection
+let accountCollection: Collection
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
@@ -13,6 +16,8 @@ describe('Survey Routes', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('account')
+    await accountCollection.deleteMany({})
   })
 
   afterAll(async () => {
@@ -23,6 +28,40 @@ describe('Survey Routes', () => {
     test('Should return 403 on add survey without accessToken', async () => {
       await request(app)
         .post('/api/surveys')
+        .send({
+          question: 'Question 1',
+          answers: [
+            {
+              image: 'image-1.jpg',
+              answer: 'Answer 1'
+            }
+          ]
+        })
+        .expect(403)
+    })
+
+    test('Should return 204 on add survey with accessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'jeff',
+        email: 'hi@jeff.com',
+        password: '123',
+        role: 'admin'
+      })
+
+      const id = res.ops[0]._id
+      const accessToken = sign({ id }, env.jwtSecret)
+
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
         .send({
           question: 'Question 1',
           answers: [
