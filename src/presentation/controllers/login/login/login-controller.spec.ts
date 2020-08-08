690e7850f248820f9b02e6f173921c4f38f34f42
Jefferson Tora <hi@jeffersontora.com>
2020-08-08 11:03:00 -0300

    test: ensure LoginController validation return badRequest if some validation fails

--- src/presentation/controllers/login/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login/login-controller.spec.ts
@@ -1,4 +1,4 @@
-import { serverError, unauthorized, ok } from '../../../helpers/http/http-helper'
+import { serverError, unauthorized, ok, badRequest } from '../../../helpers/http/http-helper'
 import { LoginController } from './login-controller'
 import {
   Validation,
@@ -6,6 +6,7 @@ import {
   Authentication,
   AuthenticationModel
 } from './login-controller-protocols'
+import { MissingParamError } from '../../../errors'
 
 const makeAuthentication = (): Authentication => {
   class AuthenticationStub implements Authentication {
@@ -85,5 +86,20 @@ describe('Login Controller', () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(ok({ accessToken: 'any_token' }))
+  })
+
+  test('Should call Validation with correct value', async () => {
+    const { sut, validationStub } = makeSut()
+    const validateSpy = jest.spyOn(validationStub, 'validate')
+    const httpRequest = makeFakeRequest()
+    await sut.handle(httpRequest)
+    expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
   })
 })
