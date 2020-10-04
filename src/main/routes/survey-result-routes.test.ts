cda1745b5f2c528375a2e2c999c64b4495b09279
Jefferson Tora <hi@jeffersontora.com>
2020-10-04 10:40:43 -0300

    test: ensure load survey route returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -86,5 +86,28 @@ describe('Survey Routes', () => {
         .get('/api/surveys/any_id/results')
         .expect(403)
     })
+
+    test('Should return 200 on load survey result with accessToken', async () => {
+      const accessToken = await makeAccessToken()
+      const res = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [
+          {
+            answer: 'Answer 1',
+            image: 'image-1.jpg'
+          },
+          {
+            answer: 'Answer 2',
+            image: 'image-2.jpg'
+          }
+        ],
+        date: new Date()
+      })
+
+      await request(app)
+        .get(`/api/surveys/${res.ops[0]._id}/results`)
+        .set('x-access-token', accessToken)
+        .expect(200)
+    })
   })
 })
