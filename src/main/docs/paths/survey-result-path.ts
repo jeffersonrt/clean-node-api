9f1d00c7ae836fcef69db0c6f0978f66fce1b155
Jefferson Tora <hi@jeffersontora.com>
2020-10-04 10:45:35 -0300

    docs: add load survey result path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -2,6 +2,7 @@ export const surveyResultPath = {
   put: {
     tags: ['Enquete'],
     summary: 'API para criar a resposta de uma enquete',
+    description: 'Essa rota só pode ser executada por **usuários autenticados**',
     security: [{
       apiKeyAuth: []
     }],
@@ -14,6 +15,44 @@ export const surveyResultPath = {
         }
       }
     },
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  },
+
+  get: {
+    tags: ['Enquete'],
+    summary: 'API para consultar o resultado de uma enquete',
+    description: 'Essa rota só pode ser executada por **usuários autenticados**',
+    security: [{
+      apiKeyAuth: []
+    }],
     parameters: [{
       in: 'path',
       name: 'surveyId',
